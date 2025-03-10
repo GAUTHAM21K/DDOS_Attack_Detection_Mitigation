aaf783535b764bfa3018a0be4b78c4158758b065
GAUTHAM21K <156946280+GAUTHAM21K@users.noreply.github.com>
2025-03-10 10:46:39 +0530

    Update KNN_mit.py

--- controller/KNN_mit.py
+++ controller/KNN_mit.py
@@ -12,28 +12,32 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 
-class SimpleMonitor13(switch.SimpleSwitch13):
+class IntegratedMonitor(switch.SimpleSwitch13):
 
     def __init__(self, *args, **kwargs):
-        super(SimpleMonitor13, self).__init__(*args, **kwargs)
+        super(IntegratedMonitor, self).__init__(*args, **kwargs)
         self.datapaths = {}
         self.monitor_thread = hub.spawn(self._monitor)
+        self.mitigation = 0
 
         start = datetime.now()
-        self.flow_training()
-        end = datetime.now()
-        print("Training time: ", (end-start))
 
-    @set_ev_cls(ofp_event.EventOFPStateChange, [MAIN_DISPATCHER, DEAD_DISPATCHER])
+        self.flow_training()
+
+        end = datetime.now()
+        print("Training time: ", (end - start))
+
+    @set_ev_cls(ofp_event.EventOFPStateChange,
+                [MAIN_DISPATCHER, DEAD_DISPATCHER])
     def _state_change_handler(self, ev):
         datapath = ev.datapath
         if ev.state == MAIN_DISPATCHER:
             if datapath.id not in self.datapaths:
-                self.logger.debug('register datapath: %016x', datapath.id)
+                self.logger.debug('Register datapath: %016x', datapath.id)
                 self.datapaths[datapath.id] = datapath
         elif ev.state == DEAD_DISPATCHER:
             if datapath.id in self.datapaths:
-                self.logger.debug('unregister datapath: %016x', datapath.id)
+                self.logger.debug('Unregister datapath: %016x', datapath.id)
                 del self.datapaths[datapath.id]
 
     def _monitor(self):
@@ -41,72 +45,82 @@ class SimpleMonitor13(switch.SimpleSwitch13):
             for dp in self.datapaths.values():
                 self._request_stats(dp)
             hub.sleep(10)
+
             self.flow_predict()
 
     def _request_stats(self, datapath):
-        self.logger.debug('send stats request: %016x', datapath.id)
+        self.logger.debug('Send stats request: %016x', datapath.id)
         parser = datapath.ofproto_parser
+
         req = parser.OFPFlowStatsRequest(datapath)
         datapath.send_msg(req)
 
     @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
     def _flow_stats_reply_handler(self, ev):
-        # Same as earlier, collecting flow stats
+        # Handle and save stats as in both codes
+        # (Combine the logic for writing PredictFlowStatsfile.csv if necessary)
+
+        # Similar flow-stats processing logic as before
         pass
 
     def flow_training(self):
-        # Same as earlier, training the ML model
-        pass
+        self.logger.info("Flow Training ...")
+
+        flow_dataset = pd.read_csv('FlowStatsfile.csv')
+
+        # Replacing '.' characters in specific columns
+        flow_dataset.iloc[:, 2] = flow_dataset.iloc[:, 2].str.replace('.', '')
+        flow_dataset.iloc[:, 3] = flow_dataset.iloc[:, 3].str.replace('.', '')
+        flow_dataset.iloc[:, 5] = flow_dataset.iloc[:, 5].str.replace('.', '')
+
+        X_flow = flow_dataset.iloc[:, :-1].values.astype('float64')
+        y_flow = flow_dataset.iloc[:, -1].values
+
+        X_flow_train, X_flow_test, y_flow_train, y_flow_test = train_test_split(X_flow, y_flow, test_size=0.25, random_state=0)
+
+        classifier = KNeighborsClassifier(n_neighbors=5, metric='minkowski', p=2)
+        self.flow_model = classifier.fit(X_flow_train, y_flow_train)
+
+        y_flow_pred = self.flow_model.predict(X_flow_test)
+
+        self.logger.info("Confusion Matrix:")
+        cm = confusion_matrix(y_flow_test, y_flow_pred)
+        self.logger.info(cm)
+
+        acc = accuracy_score(y_flow_test, y_flow_pred)
+        self.logger.info("Success Accuracy = {0:.2f}%".format(acc * 100))
+        fail = 1.0 - acc
+        self.logger.info("Fail Accuracy = {0:.2f}%".format(fail * 100))
 
     def flow_predict(self):
         try:
             predict_flow_dataset = pd.read_csv('PredictFlowStatsfile.csv')
 
+            # Replacing '.' characters in specific columns
             predict_flow_dataset.iloc[:, 2] = predict_flow_dataset.iloc[:, 2].str.replace('.', '')
             predict_flow_dataset.iloc[:, 3] = predict_flow_dataset.iloc[:, 3].str.replace('.', '')
             predict_flow_dataset.iloc[:, 5] = predict_flow_dataset.iloc[:, 5].str.replace('.', '')
 
-            X_predict_flow = predict_flow_dataset.iloc[:, :].values
-            X_predict_flow = X_predict_flow.astype('float64')
-            
+            X_predict_flow = predict_flow_dataset.iloc[:, :].values.astype('float64')
             y_flow_pred = self.flow_model.predict(X_predict_flow)
 
-            legitimate_traffic = 0
-            ddos_traffic = 0
+            legitimate_traffic = sum(y_flow_pred == 0)
+            ddos_traffic = sum(y_flow_pred == 1)
 
-            for i in range(len(y_flow_pred)):
-                if y_flow_pred[i] == 0:
-                    legitimate_traffic += 1
-                else:
-                    ddos_traffic += 1
-                    victim = int(predict_flow_dataset.iloc[i, 5]) % 20
-
-                    # Mitigation logic: Block malicious traffic
-                    self.logger.info("Blocking malicious traffic...")
-                    self._block_traffic(predict_flow_dataset.iloc[i, 3], predict_flow_dataset.iloc[i, 5])
-
+            self.logger.info("Traffic Analysis:")
             if (legitimate_traffic / len(y_flow_pred) * 100) > 80:
                 self.logger.info("Traffic is Legitimate!")
             else:
-                self.logger.info("NOTICE!! DoS Attack Detected!!!")
-                self.logger.info(f"Victim Host: h{victim}")
+                self.logger.info("NOTICE!! DoS Attack in Progress!!!")
+                victim = int(predict_flow_dataset.iloc[ddos_traffic - 1, 5]) % 20  # Example victim calculation
+                self.logger.info("Victim Host: h{}".format(victim))
+                self.mitigation = 1
+                self.logger.info("Mitigation in Progress!")
 
         except Exception as e:
-            self.logger.error(f"Error in flow prediction: {e}")
+            self.logger.error("Error in flow prediction: {}".format(e))
 
-    def _block_traffic(self, ip_src, ip_dst):
-        for dp in self.datapaths.values():
-            parser = dp.ofproto_parser
-            ofproto = dp.ofproto
-
-            match = parser.OFPMatch(eth_type=0x0800, ipv4_src=ip_src, ipv4_dst=ip_dst)
-            actions = []  # No actions mean the packets matching this flow will be dropped
-            self.add_flow(dp, 1, match, actions)
-
-    def add_flow(self, datapath, priority, match, actions):
-        ofproto = datapath.ofproto
-        parser = datapath.ofproto_parser
-
-        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
-        mod = parser.OFPFlowMod(datapath=datapath, priority=priority, match=match, instructions=inst)
-        datapath.send_msg(mod)
+        # Reset the file after processing
+        open("PredictFlowStatsfile.csv", "w").write(
+            'timestamp,datapath_id,flow_id,ip_src,tp_src,ip_dst,tp_dst,ip_proto,icmp_code,icmp_type,flow_duration_sec,flow_duration_nsec,idle_timeout,hard_timeout,flags,packet_count,byte_count,packet_count_per_second,packet_count_per_nsecond,byte_count_per_second,byte_count_per_nsecond\n'
+        )
